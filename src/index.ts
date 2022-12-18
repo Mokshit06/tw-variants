2b1fd5c10f150cf50c1ddf97430b73297941c8d5
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2022-12-18 18:57:58 +0530

    feat: add tw function

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,5 @@
-import { types as t, PluginObj, PluginPass } from '@babel/core';
-import { expandGroups } from 'twind';
-import fs from 'fs';
-import path from 'path';
-
-const twVariantsPath = path.join(process.cwd(), '.tw-variants', 'cache');
-
-if (fs.existsSync(twVariantsPath)) {
-  fs.unlinkSync(twVariantsPath);
-}
-
-export default function twVariantsPlugin(): PluginObj<
-  PluginPass & { twVariants: { classes: string } }
-> {
-  return {
-    name: 'tw-variants-plugin',
-    visitor: {
-      Program: {
-        enter(_, state) {
-          const twState = {
-            classes: '',
-          };
-
-          state.twVariants = twState;
-        },
-        exit(_, state) {
-          if (state.twVariants.classes.length > 0) {
-            fs.appendFileSync(twVariantsPath, state.twVariants.classes, 'utf8');
-          }
-        },
-      },
-      TaggedTemplateExpression(path, state) {
-        if (t.isIdentifier(path.node.tag) && path.node.tag.name === 'tw') {
-          const classes = expandGroups(
-            path.node.quasi.quasis.map(q => q.value.raw).join(' ')
-          );
-          state.twVariants.classes += classes;
-          state.twVariants.classes += ' ';
-          path.replaceWith(t.stringLiteral(classes));
-        }
-      },
-    },
-  };
+// Exists only for typescript
+// This function usage gets removed by the babel plugin
+export function tw(styles: TemplateStringsArray): string {
+  return styles.join('');
 }
